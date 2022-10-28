b714b065bdc3f8a6234e957d148821c7cb1ba314
Appleboyy <weeren97@gmail.com>
2022-10-28 16:12:25 +0800

    seperate functions for limit order

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,22 +1,22 @@
-const Token = artifacts.require("Token"); //deploy token contract
+const CoolToken = artifacts.require("CoolToken"); //deploy token contract
 const SecondToken = artifacts.require("SecondToken"); //deploy secondtoken contract
 const EthSwap = artifacts.require("EthSwap"); // deploy swap contract
 
 // Migration puts the smart contract on the blockchain using truffle
 module.exports = async function(deployer) {
   // Deploy Token
-  await deployer.deploy(Token);
-  const token = await Token.deployed();
+  await deployer.deploy(CoolToken);
+  const coolToken = await CoolToken.deployed();
 
   // Deploy Second Token
   await deployer.deploy(SecondToken);
   const secondToken = await SecondToken.deployed();
 
   // Deploy EthSwap
-  await deployer.deploy(EthSwap, token.address, secondToken.address);
+  await deployer.deploy(EthSwap, coolToken.address, secondToken.address);
   const ethSwap = await EthSwap.deployed();
 
   // Transfer all tokens to EthSwap
-  await token.transfer(ethSwap.address, "1000000000000000000000000");
+  await coolToken.transfer(ethSwap.address, "1000000000000000000000000");
   await secondToken.transfer(ethSwap.address, "10000000000000000000000000");
 };
